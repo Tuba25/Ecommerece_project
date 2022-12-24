9fc36f0695edbefda73af8b597f74a50fe21dd34
Sahara Alam Tuba <86379006+Tuba25@user.noreplay.github.com>
2022-12-24 21:08:57 +0600

    Third Commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { 
-  MatButtonModule,
-  } from '@angular/material/button';
-  import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconAnchor } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 const MaterialComponents=[
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatIconModule
+  
 ];
 
 @NgModule({
